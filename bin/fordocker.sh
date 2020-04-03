377c2c88921d318f68a9827c0874619d5153c0c9
Nelson Moller <nmoller.c@gmail.com>
2020-04-03 09:21:47 -0400

    We need php pdo_mysql for database actions.

--- bin/fordocker.sh
+++ bin/fordocker.sh
@@ -43,8 +43,7 @@ docker-php-ext-install -j $(nproc) intl \
        pgsql \
        soap \
        xsl \
-       xmlrpc
-
+       xmlrpc pdo_mysql
 # GD.
 docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
 docker-php-ext-install -j$(nproc) gd
